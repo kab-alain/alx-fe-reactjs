d8e9a84bf22af0fc00165bc3f61d10504faddb60
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2025-11-27 12:21:43 +0200

    Create Footer.jsx

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: '#333',
+        color: 'white',
+        textAlign: 'center',
+        padding: '15px',
+        marginTop: '20px',
+      }}
+    >
+      <p style={{ margin: 0 }}>© 2025 City Explorer App</p>
+    </footer>
+  );
+}
+
+export default Footer;
