0cc3a0d20e092b034f04d55be66e3abb64a28ed8
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2025-11-30 22:00:55 +0200

    Add SearchBar component to main App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
@@ -11,18 +12,16 @@ function App() {
         <h1>Recipe Sharing App</h1>
 
         <Routes>
-          {/* Home page: Add recipe + list */}
           <Route
             path="/"
             element={
               <>
+                <SearchBar />
                 <AddRecipeForm />
                 <RecipeList />
               </>
             }
           />
-
-          {/* Recipe details page */}
           <Route path="/recipe/:id" element={<RecipeDetails />} />
         </Routes>
       </div>
