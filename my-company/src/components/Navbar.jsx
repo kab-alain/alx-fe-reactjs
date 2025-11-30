632f2b8d71113c7fbeb775c78facfd21111df49e
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2025-11-30 21:37:54 +0200

    Create Navbar.jsx

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        gap: "20px",
+        padding: "15px",
+        backgroundColor: "#222",
+        color: "white",
+      }}
+    >
+      <Link style={{ color: "white" }} to="/">Home</Link>
+      <Link style={{ color: "white" }} to="/about">About</Link>
+      <Link style={{ color: "white" }} to="/services">Services</Link>
+      <Link style={{ color: "white" }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
