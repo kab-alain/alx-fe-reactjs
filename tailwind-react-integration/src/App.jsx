439d66181218dc09d2642b41303ee3ffc49200ac
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2025-11-30 22:44:47 +0200

    Create App.jsx

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
