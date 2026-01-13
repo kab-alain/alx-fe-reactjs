c173e8c8945e9c45ef17217445c08068faadd9c8
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2026-01-14 00:46:22 +0200

    Create App component for React Query demo

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PostsComponent from "./components/PostsComponent";
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">React Query Demo</h1>
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
