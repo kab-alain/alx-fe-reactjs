51d485550e01ef8c71fb0109ceb9237608548ca0
Alain Bertrand <115648529+kab-alain@users.noreply.github.com>
2026-01-14 00:13:44 +0200

    Add App component to render HomePage

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
